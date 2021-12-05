b1cea4d39b0fdcff5835d7f7ffccaeac6065372f
LucasLBF <lucas.fernandes1454@gmail.com>
2021-12-05 11:43:01 -0300

    add palette scale slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedPalettes[1]));
   return (
     <div className="App">
-      <Palette palette={seedPalettes[1]} />
+      <Palette palette={generatePalette(seedPalettes[1])} />
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
+import Slider, { Range } from "rc-slider";
+import "rc-slider/assets/index.css";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
 
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      level: 500,
+    };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+
+  changeLevel(level) {
+    this.setState({ level });
+  }
+
   render() {
+    const { colors } = this.props.palette;
     return (
       <div className="Palette">
+        <div className="Palette-slider">
+          <Slider
+            defaultValue={this.state.level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
         <div className="Palette-colors">
-          {this.props.palette.colors.map(colorObj => (
+          {colors[this.state.level].map(colorObj => (
             <ColorBox
-              key={colorObj.name}
-              color={colorObj.color}
+              key={colorObj.id}
+              color={colorObj.hex}
               name={colorObj.name}
             />
           ))}
