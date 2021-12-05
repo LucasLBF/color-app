ca381ae211ba60d8d9862dabe85ae7daf0e0349b
LucasLBF <lucas.fernandes1454@gmail.com>
2021-12-05 13:55:44 -0300

    create dropdown to select palette color format

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
-
+import Select from "@mui/material/Select";
+import { MenuItem } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      format: "hex",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.changeCode(e.target.value);
+  }
+
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="Navbar-brand">
@@ -22,6 +38,15 @@ class Navbar extends Component {
             step={100}
             onAfterChange={changeLevel}
           />
+        </div>
+        <div className="Navbar-select-container">
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <Select onChange={this.handleChange} value={format}>
+              <MenuItem value="hex">HEX - #ffffff</MenuItem>
+              <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+              <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1)</MenuItem>
+            </Select>
+          </FormControl>
         </div>
       </header>
     );
--- src/Palette.js
+++ src/Palette.js
@@ -8,24 +8,35 @@ class Palette extends Component {
     super(props);
     this.state = {
       level: 500,
+      format: "hex",
     };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeColorFormat = this.changeColorFormat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
   }
 
+  changeColorFormat(code) {
+    this.setState({ format: code });
+  }
+
   render() {
     const { colors } = this.props.palette;
+    const { level, format } = this.state;
     return (
       <div className="Palette">
-        <Navbar level={this.state.level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          changeCode={this.changeColorFormat}
+        />
         <div className="Palette-colors">
-          {colors[this.state.level].map(colorObj => (
+          {colors[level].map(colorObj => (
             <ColorBox
               key={colorObj.id}
-              color={colorObj.hex}
+              color={colorObj[format]}
               name={colorObj.name}
             />
           ))}
