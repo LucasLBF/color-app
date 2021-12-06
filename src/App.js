f00824e8bf99980660744d0ef34e53eb8f332f54
LucasLBF <lucas.fernandes1454@gmail.com>
2021-12-06 12:33:53 -0300

    integrate app with react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import Palette from "./Palette";
+import PaletteList from "./PaletteList";
 import seedPalettes from "./seedPalettes";
 import { generatePalette } from "./colorHelpers";
+import { Route, Switch } from "react-router-dom";
 
+const findPalette = id => seedPalettes.find(p => p.id === id);
 function App() {
-  console.log(generatePalette(seedPalettes[1]));
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedPalettes[1])} />
-    </div>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => <PaletteList paletteList={seedPalettes} />}
+      />
+      <Route
+        exact
+        path="/palette/:id"
+        render={routerProps => (
+          <Palette
+            palette={generatePalette(findPalette(routerProps.match.params.id))}
+          />
+        )}
+      />
+    </Switch>
   );
 }
 
